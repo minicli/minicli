7b6b7e9ea423026052a2ef5857e85d1ab9d05f96
Tom Benevides <eltonfelixbenevides@gmail.com>
2021-10-26 14:41:24 -0400

    addingtests

--- tests/Feature/Output/CLIThemeTest.php
+++ tests/Feature/Output/CLIThemeTest.php
@@ -3,6 +3,7 @@
 use Minicli\Output\Theme\DefaultTheme;
 use Minicli\Output\Theme\UnicornTheme;
 use Minicli\Output\CLIColors;
+use Minicli\Output\Theme\DaltonTheme;
 
 it('asserts that Default CLI theme sets all default styles', function () {
     $theme = new DefaultTheme();
@@ -38,6 +39,29 @@ it('asserts that Unicorn CLI theme sets all default styles', function () {
 
 it('asserts that missing styles in Unicorn CLI theme are included from default theme', function () {
     $theme = new UnicornTheme();
+
+    $this->assertArrayHasKey('italic', $theme->styles);
+    $this->assertArrayHasKey('bold', $theme->styles);
+    $this->assertArrayHasKey('dim', $theme->styles);
+    $this->assertArrayHasKey('underline', $theme->styles);
+    $this->assertArrayHasKey('invert', $theme->styles);
+});
+
+it('asserts that Dalton CLI theme sets all default styles', function () {
+    $theme = new DaltonTheme();
+
+    $this->assertIsArray($theme->getStyle('default'));
+    $this->assertIsArray($theme->getStyle('alt'));
+    $this->assertIsArray($theme->getStyle('info'));
+    $this->assertIsArray($theme->getStyle('info_alt'));
+    $this->assertIsArray($theme->getStyle('error'));
+    $this->assertIsArray($theme->getStyle('error_alt'));
+    $this->assertIsArray($theme->getStyle('success'));
+    $this->assertIsArray($theme->getStyle('success_alt'));
+});
+
+it('asserts that missing styles in Dalton CLI theme are included from default theme', function () {
+    $theme = new DaltonTheme();
 
     $this->assertArrayHasKey('italic', $theme->styles);
     $this->assertArrayHasKey('bold', $theme->styles);
