1445c55ca61ab662fffbae51b4e09707fecffa96
Steve McDougall <juststevemcd@gmail.com>
2023-04-28 19:18:53 +0100

    Renaming helpers to pest and type returns

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,55 +1,49 @@
 <?php
 
+declare(strict_types=1);
+
 use Minicli\App;
 use Minicli\Command\CommandCall;
 use Minicli\Command\CommandRegistry;
 
-function getCommandsPath()
+function getCommandsPath(): string
 {
     return __DIR__ . '/Assets/Command';
 }
 
-function getBasicApp()
+function getBasicApp(): App
 {
-    $config = [
+    return new App([
         'app_path' => getCommandsPath()
-    ];
-
-    return new App($config);
+    ]);
 }
 
-function getProdApp()
+function getProdApp(): App
 {
-    $config = [
+    return new App([
         'app_path' => getCommandsPath(),
         'debug' => false
-    ];
-
-    return new App($config);
+    ]);
 }
 
-function getThemedApp()
+function getThemedApp(): App
 {
-    $config = [
+    return new App([
         'app_path' => getCommandsPath(),
         'theme' => '\Unicorn',
-    ];
-
-    return new App($config);
+    ]);
 }
 
-function getCommandCall(array $parameters = null)
+function getCommandCall(array $parameters = null): CommandCall
 {
     return new CommandCall(array_merge(['minicli'], $parameters));
 }
 
-function getRegistry()
+function getRegistry(): CommandRegistry
 {
-    $config = [
+    $app = new App([
         'app_path' => getCommandsPath()
-    ];
-
-    $app = new App($config);
+    ]);
     $app->registerCommand("minicli-test", function () {
         return true;
     });
@@ -60,16 +54,14 @@ function getRegistry()
     return $registry;
 }
 
-function getRegistryWithMultiplePaths()
+function getRegistryWithMultiplePaths(): CommandRegistry
 {
-    $config = [
+    $app = new App([
         'app_path' => [
             getCommandsPath(),
             __DIR__ . '/Assets/VendorCommand'
         ]
-    ];
-
-    $app = new App($config);
+    ]);
 
     /** @var CommandRegistry $registry */
     $registry = $app->commandRegistry;
