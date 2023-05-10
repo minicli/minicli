bf089a03e0988a35a1eb54efc4809da9d23d385e
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-05-10 14:24:50 +0100

    Refactor OutputFilterTest

--- tests/Feature/Output/OutputFilterTest.php
+++ tests/Feature/Output/OutputFilterTest.php
@@ -6,52 +6,32 @@ use Minicli\Output\CLIColors;
 use Minicli\Output\Theme\UnicornTheme;
 use Minicli\Output\Filter\TimestampOutputFilter;
 
-it('asserts that SimpleOutputFilter returns unstyled content', function () {
-    $simple = new SimpleOutputFilter();
+it('asserts that SimpleOutputFilter returns unstyled content')
+    ->expect((new SimpleOutputFilter())->filter("My content"))
+    ->toBe("My content");
 
-    $text = "My content";
-    $this->assertEquals($text, $simple->filter($text));
-});
-
-it('asserts that ColorOutputFilter returns styled content with default theme', function () {
-    $color = new ColorOutputFilter();
-
-    $text = "My content";
-    $styled = $color->filter($text);
-
-    $expected =  sprintf("\e[%sm%s\e[0m", CLIColors::$FG_WHITE, $text);
-    $this->assertEquals($expected, $styled);
-});
+it('asserts that ColorOutputFilter returns styled content with default theme')
+    ->expect((new ColorOutputFilter())->filter("My content"))
+    ->toBe(sprintf("\e[%sm%s\e[0m", CLIColors::$FG_WHITE, "My content"));
 
 it('asserts that ColorOutputFilter sets theme correctly and formats with style', function () {
     $color = new ColorOutputFilter();
     $color->setTheme(new UnicornTheme());
 
-    $this->assertInstanceOf(UnicornTheme::class, $color->getTheme());
-
     $text = "My content";
     $styled = $color->filter($text, 'info');
-
     $expected =  sprintf("\e[%sm%s\e[0m", CLIColors::$FG_MAGENTA, $text);
-    $this->assertEquals($expected, $styled);
+
+    expect($color->getTheme())->toBeInstanceOf(UnicornTheme::class)
+        ->and($styled)->toBe($expected);
 });
 
-it('asserts that TimestampOutputFilter adds timestamp to messages', function () {
-    $time = new TimestampOutputFilter();
+it('asserts that TimestampOutputFilter adds timestamp to messages')
+    ->expect((new TimestampOutputFilter())->filter("test timestamp"))
+    ->toContain((new DateTime())->format('Y-m-d'))
+    ->toContain("test timestamp");
 
-    $message = "test timestamp";
-    $today = (new DateTime())->format('Y-m-d');
-
-    $this->assertStringContainsString($today, $time->filter($message));
-    $this->assertStringContainsString($message, $time->filter($message));
-});
-
-it('asserts that TimestampOutputFilter adds formatted timestamp to messages', function () {
-    $time = new TimestampOutputFilter();
-
-    $message = "test timestamp";
-    $today = (new DateTime())->format('m/d/Y');
-
-    $this->assertStringContainsString($today, $time->filter($message, 'm/d/Y'));
-    $this->assertStringContainsString($message, $time->filter($message, 'm/d/Y'));
-});
+it('asserts that TimestampOutputFilter adds formatted timestamp to messages')
+    ->expect((new TimestampOutputFilter())->filter("test timestamp", 'm/d/Y'))
+    ->toContain((new DateTime())->format('m/d/Y'))
+    ->toContain("test timestamp");
