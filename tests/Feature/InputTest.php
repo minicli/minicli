24afb2d5d720f376da96faed4513bd4f0c7d4bcd
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-05-10 13:49:43 +0100

    Refactor InputTest

--- tests/Feature/InputTest.php
+++ tests/Feature/InputTest.php
@@ -3,9 +3,8 @@
 use Minicli\Input;
 
 it('asserts that Input sets a default prompt', function () {
-    $input = new Input();
-
-    $this->assertEquals('minicli$> ', $input->getPrompt());
+    expect((new Input())->getPrompt())
+        ->toBe('minicli$> ');
 })->skip(function (): bool {
     return ! extension_loaded('readline');
 }, 'Extension readline is required.');
