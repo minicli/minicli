fa3607dc04660fbdbad32894fe8fe7f70c088dac
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-05-10 14:34:55 +0100

    Refactor ThemeHelperTest

--- tests/Feature/Output/ThemeHelperTest.php
+++ tests/Feature/Output/ThemeHelperTest.php
@@ -1,27 +1,28 @@
 <?php
 
 use Minicli\Output\Helper\ThemeHelper;
+use Minicli\Output\Theme\DefaultTheme;
+use Minicli\Output\Theme\DraculaTheme;
 use Minicli\Output\Theme\UnicornTheme;
 use Assets\Theme\CustomTheme;
 use Minicli\Output\Theme\DaltonTheme;
 
-it('asserts that ThemeHelper instantiates the right existing theme', function () {
-    $helper = new ThemeHelper('\Unicorn');
-    $filter = $helper->getOutputFilter();
+it('asserts that ThemeHelper instantiates the Default theme')
+    ->expect(fn () => (new ThemeHelper())->getOutputFilter()->getTheme())
+    ->toBeInstanceOf(DefaultTheme::class);
 
-    $this->assertInstanceOf(UnicornTheme::class, $filter->getTheme());
-});
+it('asserts that ThemeHelper instantiates the Unicorn theme')
+    ->expect(fn () => (new ThemeHelper('\Unicorn'))->getOutputFilter()->getTheme())
+    ->toBeInstanceOf(UnicornTheme::class);
 
-it('asserts that ThemeHelper instantiates Dalton theme', function () {
-    $helper = new ThemeHelper('\Dalton');
-    $filter = $helper->getOutputFilter();
+it('asserts that ThemeHelper instantiates the Dalton theme')
+    ->expect(fn () => (new ThemeHelper('\Dalton'))->getOutputFilter()->getTheme())
+    ->toBeInstanceOf(DaltonTheme::class);
 
-    $this->assertInstanceOf(DaltonTheme::class, $filter->getTheme());
-});
+it('asserts that ThemeHelper instantiates the Dracula theme')
+    ->expect(fn () => (new ThemeHelper('\Dracula'))->getOutputFilter()->getTheme())
+    ->toBeInstanceOf(DraculaTheme::class);
 
-it('asserts that ThemeHelper instantiates a custom theme', function () {
-    $helper = new ThemeHelper('Assets\Theme\Custom');
-    $filter = $helper->getOutputFilter();
-
-    $this->assertInstanceOf(CustomTheme::class, $filter->getTheme());
-});
+it('asserts that ThemeHelper instantiates a custom theme')
+    ->expect(fn () => (new ThemeHelper('Assets\Theme\Custom'))->getOutputFilter()->getTheme())
+    ->toBeInstanceOf(CustomTheme::class);
