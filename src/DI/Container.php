97eccb6becd62b31905b23462c186cb1030b1bbb
Steve McDougall <juststevemcd@gmail.com>
2023-05-12 14:19:11 +0100

    Cleaning up some of the container code

--- src/DI/Container.php
+++ src/DI/Container.php
@@ -13,43 +13,26 @@ use ReflectionNamedType;
 
 class Container implements ArrayAccess
 {
-    /**
-     * The Container Instance
-     *
-     * @var Container|null
-     */
     protected static null|Container $instance = null;
 
     /**
-     * The Container bindings, things that have been added to our container.
-     * [
-     *      'abstract class' => [
-     *          'concrete' => 'concrete class',
-     *          'shared' => 'true|false'
-     *      ]
-     * ]
-     *
      * @var array[]
      */
     protected array $bindings = [];
 
     /**
-     * Resolved Instances for the Container
-     *
      * @var object[]
      */
     private array $instances = [];
 
     /**
-     * Container constructor.
-     *
      * @return void
      */
-    private function __construct() {}
+    private function __construct()
+    {
+    }
 
     /**
-     * Get an Instance of the Container, or create a new instance
-     *
      * @return static
      */
     public static function getInstance(): static
@@ -62,12 +45,9 @@ class Container implements ArrayAccess
     }
 
     /**
-     * Bind a new item into the Container
-     *
      * @param string $abstract
      * @param Closure|string|null $concrete
      * @param bool $shared
-     *
      * @return void
      */
     public function bind(string $abstract, Closure|string|null $concrete = null, bool $shared = false): void
@@ -79,11 +59,8 @@ class Container implements ArrayAccess
     }
 
     /**
-     * Bind a singleton into the Container
-     *
      * @param string $abstract
      * @param Closure|string|null $concrete
-     *
      * @return void
      */
     public function singleton(string $abstract, Closure|string|null $concrete = null): void
@@ -92,11 +69,8 @@ class Container implements ArrayAccess
     }
 
     /**
-     * Add a new built instance into the Container, for easy access
-     *
      * @param string $abstract
      * @param mixed $instance
-     *
      * @return void
      */
     public function instance(string $abstract, mixed $instance): void
@@ -105,35 +79,24 @@ class Container implements ArrayAccess
     }
 
     /**
-     * Retrieve an item from the Container, if possible.
-     *
      * @param string $abstract
-     *
      * @return mixed
-     *
      * @throws BindingResolutionException|ReflectionException
      */
     public function make(string $abstract): mixed
     {
-        // 1. If the type has already been resolved as a singleton, just return it
         if (isset($this->instances[$abstract])) {
             return $this->instances[$abstract];
         }
 
-        // 2. Get the registered concrete resolver for this type, otherwise we'll assume we were passed a concretion that we can instantiate
         $concrete = $this->bindings[$abstract]['concrete'] ?? $abstract;
 
-        // 3. If the concrete is either a closure, or we didn't get a resolver, then we'll try to instantiate it.
         if ($concrete instanceof Closure || $concrete === $abstract) {
             $object = $this->build($concrete);
-        }
-
-        // 4. Otherwise the concrete must be referencing something else so we'll recursively resolve it until we get either a singleton instance, a closure, or run out of references and will have to try instantiating it.
-        else {
+        } else {
             $object = $this->make($concrete);
         }
 
-        // 5. If the class was registered as a singleton, we will hold the instance so we can always return it.
         if (isset($this->bindings[$abstract]) && $this->bindings[$abstract]['shared']) {
             $this->instances[$abstract] = $object;
         }
@@ -142,10 +105,7 @@ class Container implements ArrayAccess
     }
 
     /**
-     * Check if the Container contains an item.
-     *
      * @param string $abstract
-     *
      * @return bool
      */
     public function contains(string $abstract): bool
@@ -154,10 +114,7 @@ class Container implements ArrayAccess
     }
 
     /**
-     * Remove an item from the Container
-     *
      * @param string $abstract
-     *
      * @return void
      */
     public function remove(string $abstract): void
@@ -166,14 +123,9 @@ class Container implements ArrayAccess
     }
 
     /**
-     * Build an item in the Container
-     *
      * @param Closure|string $concrete
-     *
      * @return mixed
-     *
-     * @throws BindingResolutionException
-     * @throws ReflectionException
+     * @throws BindingResolutionException|ReflectionException
      */
     public function build(Closure|string $concrete): mixed
     {
@@ -194,7 +146,7 @@ class Container implements ArrayAccess
         $constructor = $reflector->getConstructor();
 
         if (is_null($constructor)) {
-            return new $concrete;
+            return new $concrete();
         }
 
         $dependencies = $constructor->getParameters();
@@ -205,13 +157,9 @@ class Container implements ArrayAccess
     }
 
     /**
-     * Build any dependencies for an item in the Container
-     *
      * @param array $dependencies
-     *
-     * @return array
-     *
-     * @throws BindingResolutionException
+     * @return array<int,mixed>
+     * @throws BindingResolutionException|ReflectionException
      */
     protected function resolveDependencies(array $dependencies): array
     {
@@ -219,7 +167,6 @@ class Container implements ArrayAccess
 
         foreach ($dependencies as $dependency) {
             // This is a much simpler version of what Laravel does
-
             $type = $dependency->getType(); // ReflectionType|null
 
             if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
@@ -233,8 +180,6 @@ class Container implements ArrayAccess
     }
 
     /**
-     * Flush the contents of the container
-     *
      * @return void
      */
     public function flush(): void
@@ -244,13 +189,10 @@ class Container implements ArrayAccess
     }
 
     /**
-     * ArrayAccess: Check if the Container contains an item.
-     *
      * @param mixed $offset
-     *
      * @return bool
      */
-    public function offsetExists($offset): bool
+    public function offsetExists(mixed $offset): bool
     {
         return $this->contains(
             abstract: $offset,
@@ -258,13 +200,9 @@ class Container implements ArrayAccess
     }
 
     /**
-     * ArrayAccess: Retrieve an item from the Container, if possible.
-     *
      * @param string $offset
-     *
      * @return mixed
-     *
-     * @throws BindingResolutionException
+     * @throws BindingResolutionException|ReflectionException
      */
     public function offsetGet($offset): mixed
     {
@@ -274,14 +212,11 @@ class Container implements ArrayAccess
     }
 
     /**
-     * ArrayAccess: Bind a new item into the Container
-     *
      * @param mixed $offset
      * @param mixed $value
-     *
      * @return void
      */
-    public function offsetSet($offset, $value): void
+    public function offsetSet(mixed $offset, mixed $value): void
     {
         $this->bind(
             abstract: $offset,
@@ -290,10 +225,7 @@ class Container implements ArrayAccess
     }
 
     /**
-     * ArrayAccess: Remove an item from the Container
-     *
      * @param string $offset
-     *
      * @return void
      */
     public function offsetUnset($offset): void
@@ -304,13 +236,9 @@ class Container implements ArrayAccess
     }
 
     /**
-     * ContainerInterface: Retrieve an item from the Container, if possible.
-     *
      * @param string $id
-     *
      * @return mixed
-     *
-     * @throws BindingResolutionException
+     * @throws BindingResolutionException|ReflectionException
      */
     public function get(string $id): mixed
     {
@@ -320,8 +248,6 @@ class Container implements ArrayAccess
     }
 
     /**
-     * ContainerInterface: Check if the Container contains an item.
-     *
      * @param string $id
      * @return bool
      */
