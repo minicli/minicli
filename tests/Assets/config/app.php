e9088180576232f13bc119caaa0c3aa38f44ccab
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-06-08 15:04:39 +0100

    Add test for app configuration

---
+++ tests/Assets/config/app.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /****************************************************************************
+     * Application Settings
+     * --------------------------------------------------------------------------
+     *
+     * These are the core settings for your application.
+     *****************************************************************************/
+
+    'app_name' => 'Configured App',
+
+    'app_path' => __DIR__.'/../Command',
+
+    'theme' => 'unicorn',
+
+    'debug' => true,
+];
