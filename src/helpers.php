312609db32ffc87630c7d5e98e833fd2439fb3d8
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-06-08 14:29:50 +0100

    Refactor App to start using the DI Container

--- src/helpers.php
+++ src/helpers.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 /**
  * @param array<string, mixed> $defaultConfig
+ * @param string $configPath
  * @return array<string, mixed>
  */
-function load_config(array $defaultConfig): array
+function load_config(array $defaultConfig, string $configPath): array
 {
     $config = [];
 
-    foreach ((array) glob(__DIR__.'/../../../../config/*.php') as $configFile) {
+    foreach ((array) glob("{$configPath}/*.php") as $configFile) {
         $configData = include $configFile;
         if (is_array($configData)) {
             $config = [...$config, ...$configData];
